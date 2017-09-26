8fcaf3dd26c77ddaa683b8e76a665f2bf7b6a8d4
longwenjie <longwenjie916@163.com>
2017-09-27 07:27:07 +0800

    初始化工程

---
+++ security-demo/src/main/java/com/lwj/security/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.lwj.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        System.out.println("=====================================begin=====================================");
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello Lwj";
+    }
+}
